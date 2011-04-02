29d70e2fbcb9d8e10f5c45e098a4f60edc8b857e
Chris Jobling <cpjobling@gmail.com>
2011-04-02 14:44:10 +0100

    Finished Chapter 5 end of chapter examples

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
   
+  # Return a logo
+  def logo
+    logo = image_tag("logo.png", :alt => "Sample Application", :class => "round")
+  end
+  
   # Return a title on a per-page basis
   def title
     base_title = "Ruby on Rails 3 Tutorial Sample Application"
