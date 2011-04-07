60dbc315d4e08a8af21652b2f5c42ced2b756470
Dr Chris P. Jobling <C.P.Jobling@Swansea.ac.uk>
2011-04-07 20:55:31 +0100

    Check in: Just completed Listing 10.1 - failing specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -159,5 +159,39 @@ describe UsersController do
 
   end
   
+  describe "GET 'edit'" do
+    before(:each) do
+      @user = Factory(:user)
+      test_sign_in(@user)
+    end
+    
+    it "should be successful" do
+      get :edit, :id => @user
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get :edit, :id => @user
+      response.should have_selector("title", :content => "Edit profile")
+    end
+    
+    it "should have a link to change the Gravatar" do
+      get :edit, :id => @user
+      gravatar_url = "http://gravatar.com/emails"
+      esponse.should have_selector("a", :href=> gravatar_url, :content => "change")
+    end
+  end
+    
+  describe "POST 'update'"  do
+    before(:each) do
+      @user = Factory(:user)
+      test_sign_in(@user)
+    end
+    
+    describe "failure"
+      
+    describe "sucesss"
+        
+  end
 
 end
